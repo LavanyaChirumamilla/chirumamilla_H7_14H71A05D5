d3f74e08473116ec3f89b933ed9187d471b596ae
LavanyaChirumamilla <32009797+LavanyaChirumamilla@users.noreply.github.com>
2017-09-16 14:45:10 +0530

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+
+	public static void main(String[] args) {
+		
+		//You may test that your code works find here
+		//Please check that your code works and has no 
+		//compilation problems before to submit
+               Student st = new Student(01,"Lakshmi",new Date(1996,01,06),63.568);
+		Student st2 = new Student(02,"Sirisha",new Date(1996,01,06),63.568);
+		Student st3 = new Student(03,"Pavani",new Date(1996,01,06),63.568);
+		Student st4 = new Student(04,"Lavanya",new Date(1996,01,06),63.568);
+	}
+
+}
